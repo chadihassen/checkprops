28b3e435ca5617bb4e137ad8813dc7b36357424e
chadihassen <hassenchadi9@gmail.com>
2023-11-06 15:10:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbarr from './components/Navbarr';
+import Players from './components/Players';
+
+
+
+function App() {
+  return (
+      <div className='vd'>
+  <Navbarr />    
+  <Players/>
+      </div>
+  );
+}
+
+export default App;
